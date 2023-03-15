79e30601e0530416f5769ac26614d90bf9c4f5f9
Mayara Paes <mpaesps133@gmail.com>
2023-03-14 21:23:36 -0300

    teste commit e push

---
+++ src/main/java/comdevsuperior/userdept/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package comdevsuperior.userdept.repositories;
+
+import comdevsuperior.userdept.entities.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
